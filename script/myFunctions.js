1b85b770f7473ed11555cab0d1c2c6aed0d0c898
Mohammad Kiani <kianimohammad@outlook.com>
2021-03-15 13:42:07 -0400

    add js functionality to about and contact page - open new window

---
+++ script/myFunctions.js
@@ -0,0 +1,20 @@
+// Thank you alert
+function tyAlert() {
+    alert("Thank you for your interest in fitness club. we will contact you soon.")
+}
+
+// Burpee video wiindow
+function burpee() {
+    window.open("script/burpee.html", "_blank", "width=610, height=360");
+    document.getElementById('video').play();
+}
+
+// Plank video window
+function plank() {
+    window.open("script/plank.html", "_blank", "width=610, height=360");
+}
+
+// Mountain Climber video window
+function mtnClimber() {
+    window.open("script/mtn.html", "_blank", "width=610, height=360");
+}
